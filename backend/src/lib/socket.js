aac47a784f61f5ce85f744c9872ac57d42a76b16
Bruno Rombi <brunorombi@yahoo.com>
2026-06-17 14:56:54 -0300

    feat: create helper function

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,8 +12,12 @@ const io = new Server(server, {
     }
 })
 
-// Store online users
 const userSocketMap = {}
+
+export function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+}
+// Store online users
 
 io.on("connection", (socket) => {
     console.log("A user connected", socket.id);
