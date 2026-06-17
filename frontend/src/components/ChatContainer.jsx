65a9ab7fdd4fb477ecd865d2f06a9d17e308d769
Bruno Rombi <brunorombi@yahoo.com>
2026-06-17 15:39:24 -0300

    feat: create useRef to scroll when receiving

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 
 import MessageSkeleton from "./skeletons/MessageSkeleton";
@@ -10,6 +10,7 @@ import { formatMessageTime } from "../lib/utils";
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeToMessages } = useChatStore();
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -17,6 +18,12 @@ const ChatContainer = () => {
 
     return () => unsubscribeToMessages();
   }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeToMessages])
+
+  useEffect(() => {
+    if(messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth"})
+    }
+  }, [messages])
 
   if (isMessagesLoading) return (
     <div className="flex-1 flex flex-col overflow-auto">
@@ -35,6 +42,7 @@ const ChatContainer = () => {
           <div
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
           >
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,8 +2,6 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 import { useAuthStore } from "./useAuthStore";
-import { use } from "react";
-
 
 export const useChatStore = create((set, get) => ({
     messages: [],
